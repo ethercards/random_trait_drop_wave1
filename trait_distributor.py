57f4fe7580cef2c77c099c4147f366a04503a705
Andras <andras@akomba.com>
2021-04-30 12:18:16 +0800

    trait distributor test mode

--- trait_distributor.py
+++ trait_distributor.py
@@ -3,47 +3,72 @@ import json
 import os
 import requests
 
-# load abi
-with open('sales_abi.json') as json_file:
-    abi = json.load(json_file)
-       
-#print(abi)
-
-# making sure we are connected
-print(w3.isConnected())
-
-# initialize contract
-
-ec = w3.eth.contract(address='0x97CA7FE0b0288f5EB85F386FeD876618FB9b8Ab8', abi=abi)
-
-
-# create records folder if necessary
+test = True
 results_folder = "results"
-if not os.path.exists(results_folder):
-    os.makedirs(results_folder)
+test_new_traits_folder = "random_drop_1"
 
 
-# loop through the 10k tokens
-# and merge the new traits with the metadata
+def load_new_traits(c):
+    # get appropriate number from rando
+    if test:
+        subfolder = os.path.join(test_new_traits_folder,(str(i % 100)))
+        filename = os.path.join(subfolder,(str(i)+".json")) 
+        new_traits = json.load(filename)
+    else:
+        new_traits_url = rando.functions.tokenURI(c).call()
+        new_traits = json.loads(requests.get(new_traits_url).text)
 
-for i in range(10000):
-    print("card",i)
-    print("")
-    # create subfolder if needed
-    subfolder = os.path.join(results_folder,(str(i % 100)))
-    if not os.path.exists(subfolder):
-        os.makedirs(subfolder)
+    return(new_traits)
 
-    # load existing metadata
-    metadata_url = ec.functions.tokenURI(i).call()
-    metadata = json.loads(requests.get(metadata_url).text)
-    print("metadata:")
-    print(metadata)
-    print("")
+def allocate_drop():
+    # load sales abi
+    with open('sales_abi.json') as json_file:
+        sales_abi = json.load(json_file)
+           
+    # load rando abi
+    with open('rando_abi.json') as json_file:
+        rando_abi = json.load(json_file)
+           
+    #print(abi)
     
-    # load new traits
-    print("new traits:")
-    print(new_trait)
-    print("")
-    # merge
-    # save merged metadata
+    # making sure we are connected
+    print(w3.isConnected())
+    
+    # initialize contract
+    
+    ec = w3.eth.contract(address='0x97CA7FE0b0288f5EB85F386FeD876618FB9b8Ab8', abi=sales_abi)
+    rando = w3.eth.contract(address='0xdc21eB6f0dacFba4dE360f3C6548A9c4B708c123', abi=rando_abi)
+    
+    
+    # create records folder if necessary
+    if not os.path.exists(results_folder):
+        os.makedirs(results_folder)
+    
+    
+    # loop through the 10k tokens
+    # and merge the new traits with the metadata
+    
+    for i in range(10000):
+        print("card",i)
+        print("")
+        # create subfolder if needed
+        subfolder = os.path.join(results_folder,(str(i % 100)))
+        if not os.path.exists(subfolder):
+            os.makedirs(subfolder)
+    
+        # load existing metadata
+        metadata_url = ec.functions.tokenURI(i).call()
+        metadata = json.loads(requests.get(metadata_url).text)
+        print("metadata:")
+        print(metadata)
+        print("")
+        
+        # load new traits
+        print("new traits:")
+        print(new_traits)
+        print("")
+        
+        # merge
+    
+        # save merged metadata
+        print("==================================")
